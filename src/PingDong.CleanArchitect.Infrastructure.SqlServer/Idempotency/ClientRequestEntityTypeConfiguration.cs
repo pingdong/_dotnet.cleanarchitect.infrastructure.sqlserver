e574b3ba8537ff129e3aded38eb2997a4ab10f00
Ping Dong <ping.dong@live.com>
2019-06-02 10:50:58 +1200

    Init Commit

---
+++ src/PingDong.CleanArchitect.Infrastructure.SqlServer/Idempotency/ClientRequestEntityTypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PingDong.CleanArchitect.Service;
+
+namespace PingDong.CleanArchitect.Infrastructure.SqlServer.Idempotency
+{
+    internal class ClientRequestEntityTypeConfiguration : IEntityTypeConfiguration<ClientRequest>
+    {
+        private readonly string _defaultSchema;
+        public ClientRequestEntityTypeConfiguration(string defaultSchema)
+        {
+            _defaultSchema = defaultSchema;
+        }
+
+        public void Configure(EntityTypeBuilder<ClientRequest> requestConfiguration)
+        {
+            requestConfiguration.ToTable("RequestsManager", _defaultSchema);
+
+            requestConfiguration.HasKey(cr => cr.Id);
+            requestConfiguration.Property(cr => cr.Name).IsRequired();
+            requestConfiguration.Property(cr => cr.Time).IsRequired();
+        }
+    }
+}
---
+++ src/PingDong.CleanArchitect.Infrastructure.SqlServer/Mediator/MediatorExtension.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PingDong.CleanArchitect.Core;
+
+namespace PingDong.CleanArchitect.Infrastructure.SqlServer
+{
+    static class MediatorExtension
+    {
+        public static async Task DispatchDomainEventsAsync<T>(this IMediator mediator, T context) where T: DbContext
+        {
+            var domainEntities = context.ChangeTracker
+                                    .Entries<Entity<T>>()
+                                    .Where(x => x.Entity.DomainEvents != null && x.Entity.DomainEvents.Any())
+                                    .ToList();
+
+            var domainEvents = domainEntities.SelectMany(x => x.Entity.DomainEvents)
+                                             .ToList();
+
+            domainEntities.ToList()
+                          .ForEach(entity => entity.Entity.ClearDomainEvents());
+
+            var tasks = domainEvents.Select(async domainEvent => {
+                                                await mediator.Publish(domainEvent);
+                                            });
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
